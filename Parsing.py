de2e62cb5922a004dbdea3cbed0abebeac489cd1
broder18 <57071377+broder18@users.noreply.github.com>
2022-06-14 16:10:26 +0300

    Update Parsing.py

--- Parsing.py
+++ Parsing.py
@@ -3,7 +3,7 @@ import numpy as np
 def parse(address):
     f = open(address, 'r')
     fstream = f.read()
-    lines = fstream.split(' ')
+    lines = fstream.split('\n')
     lines = np.array(lines)
     lines = lines.astype(int)
     f.close()
