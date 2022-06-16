506491f99ad4ad7c47cfa5b24c3a92ee7cf58a7e
broder18 <57071377+broder18@users.noreply.github.com>
2022-06-16 15:57:23 +0300

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -1,16 +1,33 @@
+from array import array
 import Parsing
+import CICFilter
 import numpy as np
 import matplotlib.pyplot as plt
 
-def show(address):
-    lines = Parsing.parse(address)
-    t = np.arange(0, len(lines), 1)
+
+def show_orig(lines):
+    plt.subplot(1, 2, 1)
     plt.ylabel('mV')
     plt.xlabel('Номер снятия отсчета')
-    plt.plot(t, lines)
+    plt.plot(lines)
     plt.grid(True)
+
+def show_decimation(lines):
+    cic = CICFilter.CicFilter(lines)
+    lines = cic.decimator(25, 10)
+    print(lines)
+    plt.subplot(1, 2, 2)
+    plt.ylabel('mV')
+    plt.xlabel('Номер снятия отсчета')
+    plt.plot(lines)
+    plt.grid(True)
+
+def start(address):
+    lines = Parsing.parse(address)
+    show_orig(lines)
+    show_decimation(lines)
     plt.show()
 
 if __name__ == "__main__":
-    show(input("File name: "))
+    start(input("File name: "))
 
